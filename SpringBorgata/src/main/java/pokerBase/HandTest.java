523d749cda440bbdd69cd1fb56f52552b247b1fd
Kelly Ann Baldino <kbaldino@udel.edu>
2015-09-28 16:06:10 -0400

    lab2

---
+++ SpringBorgata/src/main/java/pokerBase/HandTest.java
@@ -0,0 +1,94 @@
+package pokerBase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HandTest {
+
+	@Test
+	public void testHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddCardToHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCardFromHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHandDeck() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHandArrayListOfCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCards() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBestHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayerID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBestHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHandStrength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetKicker() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHighPairStrength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLowPairStrength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAce() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEvalHandArrayListOfCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEvalHand() {
+		fail("Not yet implemented");
+	}
+
+}
